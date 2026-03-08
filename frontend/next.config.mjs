3d00f589fd107ef8eb0a1e17a18cb30a8cc16200
Harshith-vm <vmharshith61441@gmail.com>
2026-03-09 01:00:48 +0530

    path fix

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://13.51.249.229:8000/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
